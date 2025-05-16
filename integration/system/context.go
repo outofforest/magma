1e4c8ca6ad916e434e5be624b578311d67dd0a56
Wojciech Malota-Wojcik <wojtek@exw.co>
2025-05-16 08:06:57 +0200

    wip

--- integration/system/context.go
+++ integration/system/context.go
@@ -3,6 +3,7 @@ package system
 import (
 	"context"
 	"testing"
+	"time"
 
 	"go.uber.org/zap"
 
@@ -11,5 +12,10 @@ import (
 
 // NewContext creates new context for testing.
 func NewContext(t *testing.T) context.Context {
-	return logger.WithLogger(t.Context(), logger.New(logger.DefaultConfig).With(zap.String("test", t.Name())))
+	ctx, cancel := context.WithTimeout(
+		logger.WithLogger(t.Context(), logger.New(logger.DefaultConfig).With(zap.String("test", t.Name()))),
+		2*time.Minute,
+	)
+	t.Cleanup(cancel)
+	return ctx
 }
